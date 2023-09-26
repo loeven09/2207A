ce75a4c61e3ce700a31bdf074e7e0d5dd4b81bd6
loeven09 <2374467686@qq.com>
2023-09-26 11:01:17 +0800

    第二次实验

---
+++ src/views/HomeView.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="home">
+    <p>今天订好了回家的日子</p>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import HelloWorld from '@/components/HelloWorld.vue'
+
+export default {
+  name: 'HomeView',
+  components: {
+    HelloWorld
+  }
+}
+</script>
